bb70983909d7920fee2dc6772d0fa430b65e7b13
gewfy <jakob@29k.org>
2023-10-17 23:08:57 +0200

    Update context with parsed values while keeping types

--- lib/ZodValidator.ts
+++ lib/ZodValidator.ts
@@ -1,44 +1,129 @@
-import { Context, Next } from 'koa';
-import { AnyZodObject, ZodEffects } from 'zod';
-import { FileRequestObjectType, ResponseType } from './Types';
+import type {
+  ExtendableContext,
+  Context,
+  Middleware,
+  DefaultState,
+  Next,
+  Request,
+} from 'koa';
+import { AnyZodObject, TypeOf, ZodEffects, ZodSchema } from 'zod';
+import { FileRequestObjectType } from './Types';
 
-export interface ZodValidatorProps {
+export type ZodValidatorProps = Props<
+  AnyZodObject | ZodEffects<AnyZodObject>,
+  AnyZodObject | ZodEffects<AnyZodObject>,
+  AnyZodObject | ZodEffects<AnyZodObject>,
+  ZodSchema,
+  FileRequestObjectType,
+  AnyZodObject | ZodEffects<AnyZodObject>,
+  ZodSchema
+>;
+interface Props<
+  TParams extends AnyZodObject | ZodEffects<AnyZodObject>,
+  TQuery extends AnyZodObject | ZodEffects<AnyZodObject>,
+  THeader extends AnyZodObject | ZodEffects<AnyZodObject>,
+  TBody extends ZodSchema,
+  TFiles extends FileRequestObjectType,
+  TFilesValidator extends AnyZodObject | ZodEffects<AnyZodObject>,
+  TResponse extends ZodSchema,
+> {
   summary?: string;
   description?: string;
-  query?: AnyZodObject | ZodEffects<AnyZodObject>;
-  params?: AnyZodObject | ZodEffects<AnyZodObject>;
-  header?: AnyZodObject | ZodEffects<AnyZodObject>;
-  body?: AnyZodObject | ZodEffects<AnyZodObject>;
-  files?: FileRequestObjectType;
-  filesValidator?: AnyZodObject | ZodEffects<AnyZodObject>;
-  response?: ResponseType;
+  query?: TQuery;
+  params?: TParams;
+  header?: THeader;
+  body?: TBody;
+  files?: TFiles;
+  filesValidator?: TFilesValidator;
+  response?: ResponseType<TResponse>;
 }
 
-export const ZodValidator = (props: ZodValidatorProps) => {
+interface ValidatedRequest<
+  TQuery extends ZodSchema,
+  THeader extends ZodSchema,
+  TBody extends ZodSchema,
+  TFilesValidator extends ZodSchema,
+> extends Request {
+  query: TypeOf<TQuery>;
+  header: TypeOf<THeader>;
+  body: TypeOf<TBody>;
+  files: TypeOf<TFilesValidator>;
+}
+
+interface ValidatedContext<
+  TParams extends ZodSchema,
+  TQuery extends ZodSchema,
+  THeader extends ZodSchema,
+  TBody extends ZodSchema,
+  TFilesValidator extends ZodSchema,
+  TResponse extends ZodSchema,
+> extends ExtendableContext {
+  request: ValidatedRequest<TQuery, THeader, TBody, TFilesValidator>;
+  params: TypeOf<TParams>;
+  body: TypeOf<TResponse>;
+}
+
+type ResponseType<TResponse extends ZodSchema> = {
+  description?: string;
+  validate?: boolean;
+  possibleStatusCodes?: number[];
+  body: TResponse;
+};
+
+export const ZodValidator = <
+  TParams extends AnyZodObject | ZodEffects<AnyZodObject>,
+  TQuery extends AnyZodObject | ZodEffects<AnyZodObject>,
+  THeader extends AnyZodObject | ZodEffects<AnyZodObject>,
+  TBody extends ZodSchema,
+  TFiles extends FileRequestObjectType,
+  TFilesValidator extends AnyZodObject | ZodEffects<AnyZodObject>,
+  TResponse extends ZodSchema,
+>(
+  props: Props<
+    TParams,
+    TQuery,
+    THeader,
+    TBody,
+    TFiles,
+    TFilesValidator,
+    TResponse
+  >,
+): Middleware<
+  DefaultState,
+  ValidatedContext<TParams, TQuery, THeader, TBody, TFilesValidator, TResponse>
+> => {
   const _ValidatorMiddleware = async (ctx: Context, next: Next) => {
     if (props.query && 'query' in ctx.request) {
-      await props.query.parseAsync(ctx.request.query);
+      const query = props.query.parse(ctx.request.query);
+
+      // Overwrites the query in the prototype so it's not stringified
+      Object.defineProperty(ctx.request, 'query', {
+        value: query,
+        writable: false,
+      });
     }
     if (props.params && 'params' in ctx) {
-      await props.params.parseAsync(ctx.params);
+      ctx.params = props.params.parse(ctx.params);
     }
     if (props.header) {
-      await props.header.parseAsync(ctx.request.header);
+      const header = props.header.parse(ctx.request.header);
+
+      Object.defineProperty(ctx.request, 'header', {
+        value: header,
+        writable: false,
+      });
     }
     if (props.body && 'body' in ctx.request) {
-      await props.body.parseAsync(ctx.request.body);
+      ctx.request.body = props.body.parse(ctx.request.body);
     }
     if (props.filesValidator && 'files' in ctx.request) {
-      await props.filesValidator.parseAsync(ctx.request.files);
+      ctx.request.files = props.filesValidator.parse(ctx.request.files);
     }
-    if (props.response?.validate) {
-      return next().then(async () => {
-        if (props.response) {
-          await props.response.body.parseAsync(ctx.body);
-        }
-      });
-    } else {
-      return next();
+
+    await next();
+
+    if (props.response?.validate && ctx.status === 200 && ctx.body) {
+      ctx.body = props.response.body.parse(ctx.body);
     }
   };
   _ValidatorMiddleware._VALIDATOR_PROPS = props;
